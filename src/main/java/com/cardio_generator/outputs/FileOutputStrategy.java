fbaef1d1aa7ca58fcaf3bacaaaffbe032504a066
epnck <emma.pinckers@gmail.com>
2024-04-22 15:24:19 +0200

    Added javadoc for FileOutputStrategy

--- src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/FileOutputStrategy.java
@@ -7,6 +7,16 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * The FileOutputStrategy class represents an output strategy that writes data into files.
+ * It creates a file that contains all relevant data of patients.
+ * For each patient this consists of patient ID, the timestamp when the data was taken,
+ * the label of the data, and the actual health data.
+ * <p>
+ * This class implements the OutputStrategy interface
+ *
+ * @author Tom Pepels
+ */
 // Changed class name to UpperCamelCase
 public class FileOutputStrategy implements OutputStrategy {
 
@@ -15,11 +25,24 @@ public class FileOutputStrategy implements OutputStrategy {
     // Changed variable name to camelCase
     public final ConcurrentHashMap<String, String> fileMap = new ConcurrentHashMap<>();
 
+    /**
+     * Constructs a new FileOutputStrategy with the specified base directory.
+     *
+     * @param baseDirectory The base directory where files will be stored.
+     */
     public FileOutputStrategy(String baseDirectory) {
         // Removed blank line for readability
         this.baseDirectory = baseDirectory;
     }
 
+    /**
+     * Outputs the provided data for a specified patient into a file containing all patient data.
+     *
+     * @param patientId The ID of the patient to output data for.
+     * @param timestamp The timestamp associated with the data.
+     * @param label The label corresponding to the data type.
+     * @param data The data to be outputted.
+     */
     @Override
     public void output(int patientId, long timestamp, String label, String data) {
         try {
